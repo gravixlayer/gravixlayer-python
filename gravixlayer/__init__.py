e4fa0e37484ed848d8f3da96145cdaab3069aea8
Sukrith <sukrithpvs2005@gmail.com>
2025-09-17 16:23:54 +0530

    Bump version: 0.0.28 → 0.0.29

--- gravixlayer/__init__.py
+++ gravixlayer/__init__.py
@@ -1,7 +1,7 @@
 """
 GravixLayer Python SDK - Industry Standard Compatible
 """
-__version__ = "0.0.28"
+__version__ = "0.0.29"
 
 from .client import GravixLayer
 from .types.async_client import AsyncGravixLayer
