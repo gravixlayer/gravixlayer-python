52e673270626451e093737ab5aace735d74ff4af
sukrithpvs <sukrithpvs@users.noreply.github.com>
2025-10-30 12:11:21 +0000

    Bump version: 0.0.46 -> 0.0.47

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 
 """Version information for GravixLayer SDK"""
 
-__version__ = "0.0.46"
+__version__ = "0.0.47"
 __version_info__ = tuple(int(x) for x in __version__.split('.'))
 
 # Version history
